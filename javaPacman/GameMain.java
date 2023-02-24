356962b7cea0e237e44a0661a9b5f1b84ce63d3f
kimgyeongeun117 <125328301+kimgyeongeun117@users.noreply.github.com>
2023-02-24 17:51:10 +0900

    enemy

---
+++ javaPacman/GameMain.java
@@ -0,0 +1,15 @@
+package javaPacman;
+
+import javax.swing.JFrame;
+
+public class GameMain extends JFrame{
+	
+	public static Map map;
+
+	public static void main(String[] args) {		
+		map = new Map("images/map.png");
+		
+
+	}
+
+}
